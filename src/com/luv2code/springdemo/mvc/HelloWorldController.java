56accc7153ed37180e365bdc7aa96b2b087686c0
Ankit <ankitthakur1998@gmail.com>
2022-04-15 16:55:31 +0530

    HttpServletRequest & Model interfaces are used

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller // This tells spring, hey spring this is a MVC Controller
@@ -21,6 +24,21 @@ public class HelloWorldController {
 		
 	}
 	
+	//now a controller method to read form data & 
+	//add data to the model 
 	
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		//read the request parameter from the HTML form
+		
+		//Convert the data to all UpperCase
+		
+		//Create the message
+		
+		//add message to the model
+		return "helloworld";
+		
+	}
 	
 }
