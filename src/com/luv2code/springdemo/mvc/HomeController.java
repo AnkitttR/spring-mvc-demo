d41e601d1a9dff5d7c8c9af9e9a3a6bbb1584589
Ankit <ankitthakur1998@gmail.com>
2022-04-15 12:51:09 +0530

    main-menu.jsp file created in view folder

--- src/com/luv2code/springdemo/mvc/HomeController.java
+++ src/com/luv2code/springdemo/mvc/HomeController.java
@@ -1,5 +1,16 @@
 package com.luv2code.springdemo.mvc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller    // This tells Spring, hey Spring this is a MVC Controller
 public class HomeController {
 
+	@RequestMapping("/")   //Url request will execute this mapped method
+	public String showPage() {
+		return "main-menu";	// This method will show main-menu web page
+	}
+	
+	
+	
 }
