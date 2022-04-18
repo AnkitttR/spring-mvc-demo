cb39cfe1aa3bbcfe39ecd60bc39cc3787719c0a4
Ankit <ankitthakur1998@gmail.com>
2022-04-18 16:24:59 +0530

    @Valid used in CustomerController class

--- src/com/luv2code/springdemo/mvc/CustomerController.java
+++ src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,20 +1,32 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
-	@RequestMapping("/showForm")
+	@RequestMapping("/showForm") // Lead to helloworld-form.jsp
 	public String showForm(Model theModel) {
 	
+	//for customer-form, Model attribute name is customer & value is new Customer().
 	theModel.addAttribute("customer", new Customer());
 	
 	return "customer-form";
-	
-	
+		
 }
+	@RequestMapping("/processForm")  
+	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
+			BindingResult theBindingResult) {
+		
+		return "customer-confirmation";
+	//Spring will store results of validation in BindingResult object
+				
+	}
 }
--- src/com/luv2code/springdemo/mvc/StudentController.java
+++ src/com/luv2code/springdemo/mvc/StudentController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
 @Controller
 @RequestMapping("/student")  //Parent Mapping
 public class StudentController {
