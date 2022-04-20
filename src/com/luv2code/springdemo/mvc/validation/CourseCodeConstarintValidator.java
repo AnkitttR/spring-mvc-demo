ded7aed271d18e4089706db4cf7f24080f2e9523
Ankit <ankitthakur1998@gmail.com>
2022-04-20 09:06:52 +0530

    CourseCodeConstarintValidator modified

--- src/com/luv2code/springdemo/mvc/validation/CourseCodeConstarintValidator.java
+++ src/com/luv2code/springdemo/mvc/validation/CourseCodeConstarintValidator.java
@@ -14,9 +14,18 @@ public class CourseCodeConstarintValidator implements ConstraintValidator<Course
 
 	//Spring MVC will call : isValid()
 	@Override
-	public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
+	public boolean isValid(String theCode, 
+			ConstraintValidatorContext theConstraintValidatorContext) {
 		
-		boolean result = theCode.startsWith(coursePrefix);
+		// Test if form data starts with course prefix
+		boolean result;
+		
+		if(theCode!= null) {
+		result = theCode.startsWith(coursePrefix);
+		}
+		else {
+			result = true;
+		}
 		
 		return false;
 	}
