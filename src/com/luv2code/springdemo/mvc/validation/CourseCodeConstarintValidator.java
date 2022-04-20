3275bb8db1f9bc90919406cbcf870f1aab0203f5
Ankit <ankitthakur1998@gmail.com>
2022-04-20 08:31:00 +0530

    methods overrided in CourseCodeConstarintValidator

--- src/com/luv2code/springdemo/mvc/validation/CourseCodeConstarintValidator.java
+++ src/com/luv2code/springdemo/mvc/validation/CourseCodeConstarintValidator.java
@@ -1,7 +1,22 @@
 package com.luv2code.springdemo.mvc.validation;
 
 import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
 
 public class CourseCodeConstarintValidator implements ConstraintValidator<CourseCode, String> {
 
+	@Override
+	public void initialize(CourseCode constraintAnnotation) {
+		// TODO Auto-generated method stub
+		ConstraintValidator.super.initialize(constraintAnnotation);
+	}
+
+	@Override
+	public boolean isValid(String arg0, ConstraintValidatorContext arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	
 }
