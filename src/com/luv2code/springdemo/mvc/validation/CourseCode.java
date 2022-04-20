cbb0f9c210f568e008c4ac507bfb5332bd334840
Ankit <ankitthakur1998@gmail.com>
2022-04-20 07:42:03 +0530

    @Constarint,@Target,@Retention annotation used for our custom annotation CourseCode

--- src/com/luv2code/springdemo/mvc/validation/CourseCode.java
+++ src/com/luv2code/springdemo/mvc/validation/CourseCode.java
@@ -1,5 +1,19 @@
 package com.luv2code.springdemo.mvc.validation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+
+// CourseCodeConstarintValidator.class is helper class that contains validation logic
+//@Target tells about the target where to apply the custom annotation CourseCode, i.e can apply
+// custom annotation to method or field
+//@Retention is for how long the custom annotation will bw stored or used
+@Constraint(validatedBy = CourseCodeConstarintValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface CourseCode {
 
 }
