f1748f4b5c346e9ab3c7b0e6e623f10d01f3a89f
Ankit <ankitthakur1998@gmail.com>
2022-04-20 08:35:08 +0530

    initialize method changed in CourseCodeConstarintValidator

--- src/com/luv2code/springdemo/mvc/validation/CourseCodeConstarintValidator.java
+++ src/com/luv2code/springdemo/mvc/validation/CourseCodeConstarintValidator.java
@@ -5,10 +5,11 @@ import javax.validation.ConstraintValidatorContext;
 
 public class CourseCodeConstarintValidator implements ConstraintValidator<CourseCode, String> {
 
+	private String coursePrefix;
+	
 	@Override
-	public void initialize(CourseCode constraintAnnotation) {
-		// TODO Auto-generated method stub
-		ConstraintValidator.super.initialize(constraintAnnotation);
+	public void initialize(CourseCode theCourseCode) {
+		coursePrefix = theCourseCode.value();
 	}
 
 	@Override
